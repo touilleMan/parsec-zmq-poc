51bdf485f23645e8836d539aa648cdce692c7ef7
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-10-19 18:10:40 +0200

    Clean front

--- parsec/front.py
+++ parsec/front.py
@@ -1,33 +1,25 @@
 import zmq
-import random, string
 import readline
 import json
 from pprint import pprint
-
-
-def _unique_enough_id():
-    # Colision risk is high, but this is pretty fine (and much more readable
-    # than a uuid4) for giving id to connections
-    return ''.join([random.choice(string.ascii_letters + string.digits) for ch in range(4)])
 
 
 def main(addr):    
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     socket.connect(addr)
-    conn_id = _unique_enough_id()
 
     while True:
-        cmd = input('%s>> ' % conn_id)
+        cmd = input('>> ')
         if cmd in ['q', 'quit']:
             print('bye ;-)')
             return False  # Don't restart main
         try:
-            msg = {**json.loads(cmd), 'conn_id': conn_id}
+            msg = json.loads(cmd)
         except:
             try:
                 cmd, path = cmd.split()
-                msg = {'cmd': cmd, 'path': path, 'conn_id': conn_id}
+                msg = {'cmd': cmd, 'path': path}
             except:
                 print('Invalid format')
                 continue
